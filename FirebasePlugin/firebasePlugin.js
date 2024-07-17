702461c8f2f5dfd4f050efbd443ec27c49748ed2
KYPost <kypostdesign@gmail.com>
2024-07-17 12:18:39 +0800

    initial commit

---
+++ FirebasePlugin/firebasePlugin.js
@@ -0,0 +1,14 @@
+// Firebase的金鑰
+const firebaseConfig = {
+  apiKey: "AIzaSyBADsimEazvPtW06hXyTZB-V6CQFTktJe8",
+  authDomain: "mainstage-f5ee7.firebaseapp.com",
+  databaseURL: "https://mainstage-f5ee7-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mainstage-f5ee7",
+  storageBucket: "mainstage-f5ee7.appspot.com",
+  messagingSenderId: "431203044558",
+  appId: "1:431203044558:web:ce4c4ab3aac20e9949d49f",
+  measurementId: "G-66L9C3EKZL"
+};
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
